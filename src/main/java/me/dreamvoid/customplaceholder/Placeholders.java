50e460e94af4d423a585c6867cbb25db32de6309
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-05-23 22:09:45 +0800

    初步代码

---
+++ src/main/java/me/dreamvoid/customplaceholder/Placeholders.java
@@ -0,0 +1,40 @@
+package me.dreamvoid.customplaceholder;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Placeholders extends PlaceholderExpansion {
+
+	private final main plugin;
+
+	public Placeholders(main plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public @NotNull String getIdentifier() {
+		return "customp";
+	}
+
+	@Override
+	public @NotNull String getAuthor() {
+		return "DreamVoid";
+	}
+
+	@Override
+	public @NotNull String getVersion() {
+		return "1.0";
+	}
+
+	@Override
+	public boolean persist() {
+		return true;
+	}
+
+	@Override
+	public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+		return plugin.getConfig().getString("placeholders." + params);
+	}
+}
